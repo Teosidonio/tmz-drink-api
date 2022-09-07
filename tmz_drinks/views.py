9d97180794221635df867a9f667d05ac8b6c92a1
a241085 <Teodorico.Mazivila@standardbank.co.mz>
2022-09-07 18:00:35 +0200

    Move project files to repo

---
+++ tmz_drinks/views.py
@@ -0,0 +1,46 @@
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+#Define a Get request for the API -> Starting with a decorator
+@api_view(['GET', 'POST'])
+def drink_list(request, format=None):
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return Response(serializer.data)
+
+# Deinfing the POST Method ("We are adding the create view to allow the API to post into the databse")    
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+# Defining the detailed view and setting the create update and Delete
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, id, format=None):
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+
+    # Check if this is a post method and proceed to create if true
+    elif request.method == 'PUT':
+        serializer = DrinkSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Delete if the required method is destroy
+    elif request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
