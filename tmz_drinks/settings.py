e70e1c21c335e03329783171fd1fbe983f915073
a241085 <Teodorico.Mazivila@standardbank.co.mz>
2022-09-07 20:02:21 +0200

    removed secrets

--- tmz_drinks/settings.py
+++ tmz_drinks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y&7@577liluht=f7=spbjofs$jz)u6*3ev6gsygn(=$()1jpb0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework_swagger',
     'rest_framework',
     'tmz_drinks',
     'django.contrib.admin',
